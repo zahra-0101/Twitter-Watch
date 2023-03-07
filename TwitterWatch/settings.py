fb4ffd3d62307f6fe0beb97a842f03ffd2e7e2d5
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-07 12:30:39 +0330

    delete whitenoise

--- TwitterWatch/settings.py
+++ TwitterWatch/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'TwitterWatch.urls'
@@ -131,7 +131,6 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
