44443e258ed99e40e459a46a190fc1f0c9fe7a16
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-09 17:31:44 +0330

    requirnments modified migrations

--- api/management/commands/get_user_threads_based_date.py
+++ api/management/commands/get_user_threads_based_date.py
@@ -1,9 +1,13 @@
+# This script updates tweets for all users in the database.
+
 from django.core.management.base import BaseCommand
-from accounts.views import get_user_threads_since_date
+
+from accounts.views import update_tweets_for_user
+
 
 class Command(BaseCommand):
     help = 'Updates the accounts in the database'
 
     def handle(self, *args, **options):
-        get_user_threads_since_date()
+        update_tweets_for_user()
         self.stdout.write(self.style.SUCCESS('Successfully updated the accounts'))
