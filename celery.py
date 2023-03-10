649c823796bb8aa107753eeb3a921de25c97cedb
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-10 19:36:17 +0200

    celery directory changed

--- celery.py
+++ celery.py
@@ -1,25 +1,29 @@
-# from __future__ import absolute_import, unicode_literals
-# import os
-# from celery import Celery
-# from celery.schedules import crontab
-
-# # set the default Django settings module for the 'celery' program.
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+# celery.py
+import os
+from celery import Celery
+from celery.schedules import crontab
 
 
-# app = Celery('TwitterWatch')
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwitterWatch.settings')
 
-# # Using a string here means the worker will not have to
-# # pickle the object when using Windows.
-# app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('TwitterWatch')
 
-# # Load task modules from all registered Django app configs.
-# app.autodiscover_tasks()
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# # Define a periodic task that runs every 15 minutes.
-# app.conf.beat_schedule = {
-#     'run-task-one-every-15-minutes': {
-#         'task': 'accounts.tasks.get_user_threads_since_id_task',
-#         'schedule': crontab(minute='*/60')
-#     },
-# }
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+# define a periodic task to print the text every 1 minute
+@app.task
+def print_text():
+    print("this is a test")
+
+app.conf.beat_schedule = {
+    'print-text-every-1-minute': {
+        'task': 'myproject.tasks.print_text',
+        'schedule': crontab(minute='*/1'),
+    },
+}
