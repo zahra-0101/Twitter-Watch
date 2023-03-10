c76a318d103c6d097c8c739d025327aa5ced0f54
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-10 11:05:12 +0330

    vercel tasks added

---
+++ accounts/tasks.py
@@ -0,0 +1,15 @@
+# from __future__ import absolute_import, unicode_literals
+# from celery import shared_task
+# from datetime import datetime, timedelta
+# from django.utils import timezone
+# from .views import get_user_threads_since_id
+
+# @shared_task
+# def get_user_threads_since_id_task():
+#     get_user_threads_since_id()
+
+# @shared_task
+# def task_two():
+#     # This task can be run anytime by user
+#     # Perform the necessary operations here
+#     pass
