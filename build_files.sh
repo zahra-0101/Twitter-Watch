29104296d22ad504db03073d5621cd77c44c588f
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-10 11:41:46 +0330

    build update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 # build_files.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-python3.9 manage.py get_user_threads_based_date
+# python3.9 manage.py get_user_threads_based_date
