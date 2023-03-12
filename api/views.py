5c63d15a3c83b7f604ae670df40b5ab6da6ffba7
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-12 11:41:08 +0330

    nltk deleted

--- api/views.py
+++ api/views.py
@@ -14,18 +14,15 @@ from .serializers import (AudienceInfoSerializer, TwitterAccountSerializer,
 
 import snscrape.modules.twitter as sntwitter
 from accounts.views import get_all_replies_belong_to_the_last_n_tweets
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
-import nltk
+from textblob import TextBlob
 
 # Download the VADER lexicon if it is not already installed
-nltk.download('vader_lexicon')
 
 def sentiment_analyzer(text):
 
-    analyzer = SentimentIntensityAnalyzer()
-    sentiment_score = analyzer.polarity_scores(text)
-    return sentiment_score['compound']
+    blob = TextBlob(text)
+    sentiment_score = blob.sentiment
+    return sentiment_score
 
 
 class TwitterAccountList(generics.ListCreateAPIView):
@@ -58,8 +55,8 @@ class AudienceInfoAPIView(generics.GenericAPIView ):
 
         replies, avg_likeCount, avg_quoteCount, avg_replyCount = get_all_replies_belong_to_the_last_n_tweets(twitter_handle, 5)
 
-        # sentiment_polarity = sentiment_analyzer(' '.join(replies))
-        sentiment_polarity = None
+        sentiment_polarity = sentiment_analyzer(' '.join(replies))
+
         # Serialize the audience information and return it as a response
         audience_info = {
             'followers_count': followers_count,
@@ -68,7 +65,8 @@ class AudienceInfoAPIView(generics.GenericAPIView ):
             'avg_likeCount': avg_likeCount,
             'avg_quoteCount': avg_quoteCount,
             'avg_replyCount': avg_replyCount,
-            'audience_sentiment_polarity': sentiment_polarity
+            'audience_sentiment_polarity': sentiment_polarity.polarity,
+            'audience_sentiment_subjectivity': sentiment_polarity.subjectivity,
             
         }
         serializer = AudienceInfoSerializer(audience_info)
@@ -79,36 +77,26 @@ class SentimentAPIView(generics.GenericAPIView ):
     serializer_class = AudienceInfoSerializer
 
     def get(self, request, twitter_handle, format=None):
-        # Authenticate with the Twitter API
-        config_path = os.path.join(os.path.dirname(
-            os.path.dirname(os.path.abspath(__file__))), 'config.ini')
+        
+        for i, user in enumerate(sntwitter.TwitterSearchScraper("from:{}".format(twitter_handle)).get_items()):
+            # Retrieve the audience information for the user
+            followers_count = user.user.followersCount
+            following_count = user.user.friendsCount
+            verified = user.user.verified
+            viewCount = user.viewCount
+            break
 
-        # Read the Twitter API credentials from conf/ig.ini
-        config = configparser.ConfigParser()
-        config.read(config_path)
-        consumer_key = config['TwitterAPI']['consumer_key']
-        consumer_secret = config['TwitterAPI']['consumer_secret']
-        access_token = config['TwitterAPI']['access_token']
-        access_token_secret = config['TwitterAPI']['access_token_secret']
-
-        # Authenticate with Twitter API
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-        api = tweepy.API(auth)
-        user = api.get_user(screen_name=twitter_handle)
-
-        # Retrieve the audience information for the user
-        followers_count = user.followers_count
-        following_count = user.friends_count
-        tweet_count = user.statuses_count
-        created_at = user.created_at
-        #TODO  hashtag
+        thread_level = 0
+        audience_level = 0 
+    
 
         # Serialize the audience information and return it as a response
         audience_info = {
             'followers_count': followers_count,
             'following_count': following_count,
-            'tweet_count': tweet_count,
-            'created_at': created_at,
+            'verified': verified,
+            'viewCount': viewCount,
+            
         }
         serializer = AudienceInfoSerializer(audience_info)
         return Response(serializer.data)
