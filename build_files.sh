32204613e19fb6c7a6f9b828959c6a0a37a2cc40
zahra.bahrami <z.bahrami@danalytica.com>
2023-03-12 18:32:55 +0330

    build

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-pip uninstall textblob
-pip uninstall nltk
+# pip uninstall textblob
+# pip uninstall nltk
 # python3.9 manage.py get_user_threads_based_date
